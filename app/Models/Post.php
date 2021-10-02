193db7a33c21efd1058bdcff99dee61441677f7c
Santiago Aguirre <santiago.aguirre@uosolutions.com>
2021-10-02 12:06:20 -0300

    Commit inicial. Creados los tres modelos post comment user

---
+++ app/Models/Post.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Post extends Model
+{
+    protected $fillable = [
+        'title',
+        'description',
+        'image',
+        'likes',
+        'user_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    use SoftDeletes;
+    use HasFactory;
+}
